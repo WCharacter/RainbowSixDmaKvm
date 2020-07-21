178d1bc97e01dc6545b8e0bc15e5cd6b334206d8
WCharacter <hrktr@pop-os.localdomain>
2020-07-22 02:47:36 +0300

    code refactoring, check thread removed

--- r6_dma/r6_dma.cpp
+++ r6_dma/r6_dma.cpp
@@ -6,7 +6,6 @@
 #include <random>
 #include <thread>
 #include <chrono>
-#include <mutex>
 #include "../vmread/hlapi/hlapi.h"
 #include "offsets.h"
 
@@ -21,7 +20,6 @@ enum class FiringMode : uint8_t
 };
 
 static bool run_cheat = true;
-std::mutex mtx;
 
 struct R6Data
 {
@@ -37,9 +35,12 @@ uint64_t get_base(WinProcess &proc)
 	return peb.ImageBaseAddress;
 }
 
-void read_data(WinProcess &proc, R6Data &data)
+void read_data(WinProcess &proc, R6Data &data, bool init = true)
 {
-	data.base = get_base(proc);
+	if (init)
+	{
+		data.base = get_base(proc);
+	}
 
 	auto localplayer = proc.Read<uint64_t>(data.base + PROFILE_MANAGER_OFFSET);
 	localplayer = proc.Read<uint64_t>(localplayer + 0x68);
@@ -98,7 +99,7 @@ void enable_no_aim_animation(WinProcess &proc, const R6Data &data)
 {
 	auto no_anim = proc.Read<uint64_t>(data.localplayer + 0x90);
 	no_anim = proc.Read<uint64_t>(no_anim + 0xc8);
-	proc.Write<uint8_t>(no_anim + 0x384, 0); 
+	proc.Write<uint8_t>(no_anim + 0x384, 0);
 	printf("No aim animation active\n");
 }
 
@@ -116,136 +117,117 @@ void set_firing_mode(WinProcess &proc, const R6Data &data, FiringMode mode)
 	proc.Write<uint32_t>(data.weapon + 0x118, (uint32_t)mode); //firing mode 0 - auto 3 - single 2 -  burst
 }
 
-void write_loop()
+void write_loop(WinProcess &proc, R6Data &data)
+{
+	printf("Write thread started\n");
+	while (run_cheat)
+	{
+		R6Data old_data = data;
+
+		read_data(proc, data, false);
+		auto weapon2 = proc.Read<uint64_t>(data.weapon + 0x290) - 0x2b306cb952f73b96;
+		float spread_val = proc.Read<float>(weapon2 + 0x80);
+		if (old_data.weapon != data.weapon && spread_val != 0.f)
+		{
+			//this things should be updated every round
+			enable_no_spread(proc, data);
+			set_firing_mode(proc, data, FiringMode::AUTO);
+			enable_no_flash(proc, data);
+			enable_no_aim_animation(proc, data);
+			printf("Data updated\n");
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+	printf("Exiting...\n");
+}
+
+__attribute__((constructor)) static void init()
 {
 	pid_t pid = 0;
 #if (LMODE() != MODE_EXTERNAL())
 	pid = getpid();
 #endif
-	printf("Write thread started\n");
+	printf("Using Mode: %s\n", TOSTRING(LMODE));
 
 	try
 	{
 		R6Data data;
-		mtx.lock();
-		WinContext ctx(pid);
-		mtx.unlock();
-		while (run_cheat)
+		bool not_found = true;
+		WinContext write_ctx(pid);
+		WinContext check_ctx(pid);
+		while (not_found)
 		{
-			bool not_found = true;
-
-			while (not_found)
+			write_ctx.processList.Refresh();
+			for (auto &i : write_ctx.processList)
 			{
-				ctx.processList.Refresh();
-
-				for (auto &i : ctx.processList)
+				if (!strcasecmp("RainbowSix.exe", i.proc.name))
 				{
-					if (!strcasecmp("RainbowSix.exe", i.proc.name))
+					short magic = i.Read<short>(i.GetPeb().ImageBaseAddress);
+					auto peb = i.GetPeb();
+					auto g_Base = peb.ImageBaseAddress;
+					if (g_Base != 0)
 					{
-						short magic = i.Read<short>(i.GetPeb().ImageBaseAddress);
-						auto peb = i.GetPeb();
-						auto g_Base = peb.ImageBaseAddress;
-						if (g_Base != 0)
-						{
-							printf("\nR6 found %lx:\t%s\n", i.proc.pid, i.proc.name);
-							printf("\tBase:\t%lx\tMagic:\t%hx (valid: %hhx)\n", peb.ImageBaseAddress, magic, (char)(magic == IMAGE_DOS_SIGNATURE));
+						printf("\nR6 found %lx:\t%s\n", i.proc.pid, i.proc.name);
+						printf("\tBase:\t%lx\tMagic:\t%hx (valid: %hhx)\n", peb.ImageBaseAddress, magic, (char)(magic == IMAGE_DOS_SIGNATURE));
 
-							read_data(i, data);
+						read_data(i, data);
 
-							printf("Base: 0x%lx\nFOV Manager: 0x%lx\nLocal player: 0x%lx\nWeapon: 0x%lx\n", data.base, data.fovmanager, data.localplayer, data.weapon);
+						printf("Base: 0x%lx\nFOV Manager: 0x%lx\nLocal player: 0x%lx\nWeapon: 0x%lx\n", data.base, data.fovmanager, data.localplayer, data.weapon);
 
-							enable_esp(i, data);
-							enable_no_recoil(i, data);
-							enable_no_flash(i, data);
-							enable_no_spread(i, data);
-							enable_no_aim_animation(i, data);
-							//enable_run_and_shoot(i, data);
+						enable_esp(i, data);
+						enable_no_recoil(i, data);
+						enable_no_spread(i, data);
+						enable_no_flash(i, data);
+						enable_no_aim_animation(i, data);
+						//enable_run_and_shoot(i, data);
 
-							set_firing_mode(i, data, FiringMode::AUTO);
+						set_firing_mode(i, data, FiringMode::AUTO);
 
-							// float fov;
-							// fprintf(out, "Enter fov: ");
-							// scanf("%f", &fov);
-							// fprintf(out, "\n");
-							// set_fov(i, data, fov);
+						// float fov;
+						// fprintf(out, "Enter fov: ");
+						// scanf("%f", &fov);
+						// fprintf(out, "\n");
+						// set_fov(i, data, fov);
 
-							//checking if weapon is updated
-							while (run_cheat)
-							{
-								R6Data new_data;
-								read_data(i, new_data);
-								auto weapon2 = i.Read<uint64_t>(new_data.weapon + 0x290) - 0x2b306cb952f73b96;
-								float spread_val = i.Read<float>(weapon2 + 0x80);
-								if (new_data.weapon != data.weapon && spread_val != 0.f)
-								{
-									data = new_data;
-									//this things should be updated every round
-									enable_no_spread(i, data);							
-									set_firing_mode(i, data, FiringMode::AUTO);
-									enable_no_aim_animation(i, data);
-									printf("Data updated\n");
-								}
-								std::this_thread::sleep_for(std::chrono::milliseconds(10));
-							}
-							not_found = false;
-							break;
-						}
+						//checking if weapon is updated
+						std::thread write_thread(write_loop, std::ref(i), std::ref(data));
+						write_thread.detach();
+
+						not_found = false;
+						break;
 					}
 				}
-				std::this_thread::sleep_for(std::chrono::milliseconds(1));
 			}
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		}
+
+		while (run_cheat)
+		{
+			bool flag = false;
+			check_ctx.processList.Refresh();
+			for (auto &i : check_ctx.processList)
+			{
+				if (!strcasecmp("RainbowSix.exe", i.proc.name))
+				{
+					auto peb = i.GetPeb();
+					auto g_Base = peb.ImageBaseAddress;
+					if (g_Base != 0)
+					{
+						flag = true;
+						break;
+					}
+				}
+			}
+			if (!flag)
+				run_cheat = false;
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(50));
 		}
 	}
 	catch (VMException &e)
 	{
 		printf("Initialization error: %d\n", e.value);
 	}
-	printf("Exiting...\n");
-}
-
-void alive_check()
-{
-	pid_t pid = 0;
-#if (LMODE() != MODE_EXTERNAL())
-	pid = getpid();
-#endif
-	printf("Check thread started\n");
-	mtx.lock();
-	WinContext ctx(pid);
-	mtx.unlock();
-	while (run_cheat)
-	{		
-		bool flag = false;
-		ctx.processList.Refresh();
-		for (auto &i : ctx.processList)
-		{
-			if (!strcasecmp("RainbowSix.exe", i.proc.name))
-			{
-				auto peb = i.GetPeb();
-				auto g_Base = peb.ImageBaseAddress;
-				if (g_Base != 0)
-				{
-					flag = true;
-					break;
-				}
-			}
-		}
-		if (!flag)
-		{
-			run_cheat = false;
-		}
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(50));
-	}
-}
-
-__attribute__((constructor)) static void init()
-{
-	printf("Using Mode: %s\n", TOSTRING(LMODE));
-	std::thread check_thread(alive_check);
-	std::thread write_thread(write_loop);
-	write_thread.join();
-	check_thread.join();
 }
 
 int main()
