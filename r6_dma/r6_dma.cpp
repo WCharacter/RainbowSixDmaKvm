c220871d0812ad488197a42b25c3f2b8b50aa9f1
Hrktr <hrktr@pop-os.localdomain>
2020-07-30 16:53:17 +0300

    updated offsets

--- r6_dma/r6_dma.cpp
+++ r6_dma/r6_dma.cpp
@@ -24,12 +24,13 @@ static bool run_cheat = true;
 struct R6Data
 {
 	uint64_t base;
-	uint64_t localplayer;
-	uint64_t fovmanager;
-	uint64_t currweapon;
-	uint64_t weaponinfo;
-	uint64_t glowmanager;
-	uint64_t roundmanager;
+	uint64_t local_player;
+	uint64_t fov_manager;
+	uint64_t curr_weapon;
+	uint64_t weapon_info;
+	uint64_t glow_manager;
+	uint64_t game_manager;
+	uint64_t round_manager;
 };
 
 uint64_t get_base(WinProcess &proc)
@@ -49,59 +50,80 @@ void read_data(WinProcess &proc, R6Data &data, bool init = true)
 	localplayer = proc.Read<uint64_t>(localplayer + 0x68);
 	localplayer = proc.Read<uint64_t>(localplayer + 0x0);
 	localplayer = proc.Read<uint64_t>(localplayer + 0x28) + 0xebab0991057478ed;
-	data.localplayer = localplayer;
+	data.local_player = localplayer;
 
 	auto fov_manager = proc.Read<uint64_t>(data.base + FOV_MANAGER_OFFSET);
 	fov_manager = proc.Read<uint64_t>(fov_manager + 0xE8);
 	fov_manager = proc.Read<uint64_t>(fov_manager + 0x88B932A0D99755B8);
-	data.fovmanager = fov_manager;
+	data.fov_manager = fov_manager;
 
 	auto weapon = proc.Read<uint64_t>(localplayer + 0x90);
 	weapon = proc.Read<uint64_t>(weapon + 0xc8);
-	data.currweapon = weapon;
+	data.curr_weapon = weapon;
 
-	auto weapon2 = proc.Read<uint64_t>(data.currweapon + 0x290) - 0x2b306cb952f73b96;
-	data.weaponinfo = weapon2;
+	auto weapon2 = proc.Read<uint64_t>(data.curr_weapon + 0x290) - 0x2b306cb952f73b96;
+	data.weapon_info = weapon2;
 
-	auto glow = proc.Read<uint64_t>(data.base + GLOW_MANAGER_OFFSET);
-	data.glowmanager = glow;
+	data.glow_manager = proc.Read<uint64_t>(data.base + GLOW_MANAGER_OFFSET);
 
-	auto roundmanager = proc.Read<uint64_t>(data.base + ROUND_MANAGER_OFFSET);
-	data.roundmanager = roundmanager;
+	data.round_manager = proc.Read<uint64_t>(data.base + ROUND_MANAGER_OFFSET);
+
+	data.game_manager = proc.Read<uint64_t>(data.base + GAME_MANAGER_OFFSET); 
 }
 
 void enable_esp(WinProcess &proc, const R6Data &data)
 {
-	char shellcode[] = {(char)0xE9, (char)0x92, (char)0xFE, (char)0xFF, (char)0xFF};
-	proc.WriteMem<char>(data.base + 0x5B61E9, shellcode, sizeof(shellcode)); //cav esp
+	if(data.game_manager == 0) return;
+	auto entity_list = proc.Read<uint64_t>(data.game_manager + 0x98) + 0xE60F6CF8784B5E96;
+	if(entity_list == 0) return;	
+	for(int i = 0; i < 11; ++i)
+	{
+		auto entity_address = proc.Read<uint64_t>(entity_list + (0x8 * i)); //entity_object
+		auto buffer = proc.Read<uint64_t>(entity_address + 0x18);
+		auto size = proc.Read<uint32_t>(buffer + 0xE0) & 0x3FFFFFFF;
+		auto list_address = proc.Read<uint64_t>(buffer + 0xD8);
+		for(uint32_t i = 0; i < size; ++i)
+		{
+			auto pbuffer = proc.Read<uint64_t>(list_address + i * sizeof(uint64_t));
+			auto current_vtable_rel = proc.Read<uint64_t>(pbuffer) - data.base;
+			if(current_vtable_rel == VTMARKER_OFFSET)
+			{
+				proc.Write<uint8_t>(pbuffer + 0x632, 1);
+			}
+		}
+	}
 	printf("Cav esp active\n");
 }
 
 void enable_no_recoil(WinProcess &proc, const R6Data &data)
 {
-	proc.Write<float>(data.fovmanager + 0xE34, 0.f);
-	proc.Write<float>(data.weaponinfo + 0x198, 0.f); //rec b
-	proc.Write<float>(data.weaponinfo + 0x18c, 0.f); //rec v
-	proc.Write<float>(data.weaponinfo + 0x17c, 0.f); //rec h
+	if(data.fov_manager == 0) return;
+	proc.Write<float>(data.fov_manager + 0xE34, 0.f);
+	proc.Write<float>(data.weapon_info + 0x198, 0.f); //rec b
+	proc.Write<float>(data.weapon_info + 0x18c, 0.f); //rec v
+	proc.Write<float>(data.weapon_info + 0x17c, 0.f); //rec h
 	printf("No recoil active\n");
 }
 
 void enable_no_spread(WinProcess &proc, const R6Data &data)
 {
-	proc.Write<float>(data.weaponinfo + 0x80, 0.f); // no spread
+	if(data.weapon_info == 0) return;
+	proc.Write<float>(data.weapon_info + 0x80, 0.f); // no spread
 	printf("No spread active\n");
 }
 
 void enable_run_and_shoot(WinProcess &proc, const R6Data &data)
 {
-	proc.Write<uint8_t>(data.base + 0x2DA5D35 + 0x6, 0x1); // run and shoot
-	proc.Write<uint8_t>(data.base + 0x14765E1 + 0x6, 0x1); // run and shoot
+	if(data.base == 0) return;
+	proc.Write<uint8_t>(data.base + 0x1E59401, 0x1); // run and shoot
+	proc.Write<uint8_t>(data.base + 0x33AE195, 0x1); // run and shoot
 	printf("Run and shoot active\n");
 }
 
 void enable_no_flash(WinProcess &proc, const R6Data &data)
 {
-	auto player = proc.Read<uint64_t>(data.localplayer + 0x30);
+	if(data.local_player == 0) return;
+	auto player = proc.Read<uint64_t>(data.local_player + 0x30);
 	player = proc.Read<uint64_t>(player + 0x31);
 	auto noflash = proc.Read<uint64_t>(player + 0x28);
 	proc.Write<uint8_t>(noflash + 0x40, 0); // noflash
@@ -110,31 +132,35 @@ void enable_no_flash(WinProcess &proc, const R6Data &data)
 
 void enable_no_aim_animation(WinProcess &proc, const R6Data &data)
 {
-	auto no_anim = proc.Read<uint64_t>(data.localplayer + 0x90);
+	if(data.local_player == 0) return;
+	auto no_anim = proc.Read<uint64_t>(data.local_player + 0x90);
 	no_anim = proc.Read<uint64_t>(no_anim + 0xc8);
 	proc.Write<uint8_t>(no_anim + 0x384, 0);
-
 	printf("No aim animation active\n");
 }
 
 void set_fov(WinProcess &proc, const R6Data &data, float fov_val)
 {
 	auto fov = proc.Read<uint64_t>(data.base + FOV_MANAGER_OFFSET);
+	if(fov == 0) return;
 	fov = proc.Read<uint64_t>(fov + 0x60) + 0xe658f449242c196;
+	if(fov == 0) return;
 	auto playerfov = proc.Read<uint64_t>(fov + 0x0) + 0x38;
+	if(playerfov == 0) return;
 	proc.Write<float>(playerfov, fov_val); // player fov ~1.2f-2.3f
 	printf("Player fov changed to %f\n", fov_val);
 }
 
 void set_firing_mode(WinProcess &proc, const R6Data &data, FiringMode mode)
 {
-	proc.Write<uint32_t>(data.currweapon + 0x118, (uint32_t)mode); //firing mode 0 - auto 3 - single 2 -  burst
+	if(data.curr_weapon == 0) return; 
+	proc.Write<uint32_t>(data.curr_weapon + 0x118, (uint32_t)mode); //firing mode 0 - auto 3 - single 2 -  burst
 	printf("Fire mode: %s\n", (mode == FiringMode::AUTO ? "auto" : mode == FiringMode::BURST ? "burst" : "single"));
 }
 
 void enable_glow(WinProcess &proc, const R6Data &data)
 {
-	auto chain = proc.Read<uint64_t>(data.glowmanager + 0xb8);
+	auto chain = proc.Read<uint64_t>(data.glow_manager + 0xb8);
 	if (chain != 0)
 	{
 		proc.Write<float>(chain + 0xd0, 1.f); //r
@@ -150,23 +176,17 @@ void enable_glow(WinProcess &proc, const R6Data &data)
 		printf("[ERROR]: Can not activate glow\n");
 	}
 }
-//TODO: check for team, set color for allies and enemies
-void enable_outline(WinProcess &proc, const R6Data &data)
-{
-	//3 = defense
-	//4 = attack
-	auto chain = proc.Read<uint64_t>(data.base + GAME_MANAGER_OFFSET);
-	proc.Write<uint8_t>(chain + 0x518, 0x3);	
-}
 
 void enable_wall_ping(WinProcess &proc, const R6Data &data)
 {
+	if(data.base == 0) return;
 	proc.Write<uint8_t>(data.base + 0x19caff0, 0);
 }
 
 int32_t get_game_state(WinProcess &proc, const R6Data &data)
 {
-	return proc.Read<uint8_t>(data.roundmanager + 0x2e8);
+	if(data.round_manager == 0) return -1;
+	return proc.Read<uint8_t>(data.round_manager + 0x2e8);
 }
 
 //player is currently in operator selection menu
@@ -175,15 +195,22 @@ bool is_in_op_select_menu(WinProcess &proc, const R6Data& data)
 	return get_game_state(proc, data) == 5;
 }
 
+void unlock_all(WinProcess &proc, const R6Data& data)
+{
+	uint8_t shellcode[] = { 65, 198, 70, 81, 0, 144 };
+	proc.WriteMem(data.base + 0x271470B, shellcode, sizeof(shellcode));
+	printf("Unlock all executed\n");
+}
+
 void write_loop(WinProcess &proc, R6Data &data)
 {
-	printf("Write thread started\n");
+	printf("Write thread started\n\n");
 	while (run_cheat)
 	{
-		read_data(proc, data, data.base == 0x0 || data.fovmanager == 0x0 || data.currweapon == 0);
-		float spread_val = proc.Read<float>(data.weaponinfo + 0x80);
-				
-		if (spread_val != 0.f)
+		read_data(proc, data, data.base == 0x0 || data.fov_manager == 0x0 || data.curr_weapon == 0);
+		float spread_val = proc.Read<float>(data.weapon_info + 0x80);
+		
+		if (spread_val)
 		{
 			enable_esp(proc, data);
 			enable_no_recoil(proc, data);
@@ -213,6 +240,7 @@ __attribute__((constructor)) static void init()
 		bool not_found = true;
 		WinContext write_ctx(pid);
 		WinContext check_ctx(pid);
+		printf("Searching for a process...\n");
 		while (not_found)
 		{
 			write_ctx.processList.Refresh();
@@ -231,27 +259,29 @@ __attribute__((constructor)) static void init()
 						getchar();
 						read_data(i, data);
 
-						printf("Base: 0x%lx\nFOV Manager: 0x%lx\nLocal player: 0x%lx\nWeapon: 0x%lx\nGlow manager: 0x%lx\nRound manager: 0x%lx\n", 
-								data.base, data.fovmanager, data.localplayer, data.currweapon, data.glowmanager, data.roundmanager);
+						printf("Base: 0x%lx\nFOV Manager: 0x%lx\nLocal player: 0x%lx\nWeapon: 0x%lx\nGlow manager: 0x%lx\nRound manager: 0x%lx\nGame manager: 0x%lx\n\n", 
+								data.base, data.fov_manager, data.local_player, data.curr_weapon, data.glow_manager, data.round_manager, data.game_manager);
 
 						enable_esp(i, data);
 						enable_no_recoil(i, data);
 						enable_no_spread(i, data);
 						enable_no_flash(i, data);
 						enable_no_aim_animation(i, data);
-						enable_wall_ping(i,data);
+						//enable_wall_ping(i,data);
 						enable_glow(i, data);
-						//enable_outline(i, data);
 						//enable_run_and_shoot(i, data);
-
+					    
 						set_firing_mode(i, data, FiringMode::AUTO);
-
+						if(get_game_state(i, data) == 5)
+						{
+							unlock_all(i, data);
+						}
 						// float fov;
 						// fprintf(out, "Enter fov: ");
 						// scanf("%f", &fov);
 						// fprintf(out, "\n");
 						// set_fov(i, data, fov);
-					
+
 						std::thread write_thread(write_loop, std::ref(i), std::ref(data));
 						write_thread.detach();
 
@@ -283,7 +313,7 @@ __attribute__((constructor)) static void init()
 			if (!flag)
 				run_cheat = false;
 
-			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		}
 	}
 	catch (VMException &e)
